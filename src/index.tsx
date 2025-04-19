59c77014e17203791a6881fee7a8ecaf4f68711d
Ryan Kubik <rmkubik@me.com>
2025-04-19 10:13:37 -0700

    Migrate parcel starter to TS

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,8 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
 const root = createRoot(container);
 root.render(<App />);
